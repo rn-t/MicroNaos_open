1faaf295282ced9df3962f3b40bbe08b26507930
Raku Numata <ryaryu3ry@yahoo.co.jp>
2023-02-12 00:53:01 +0900

    flashの読み書きを実装した

---
+++ firmware/Core/Peripherals/Inc/flash.hpp
@@ -0,0 +1,13 @@
+extern char _flash_data_start;
+
+class Flash{
+    private:
+    const char* _start_address = &_flash_data_start;
+    public:
+    uint8_t is_elased = 1; 
+    uint8_t wall[16][16] __attribute__ ((aligned(4)));
+
+    void erase(void);
+    void save(uint8_t (&wall_data)[16][16]);
+    void load(void);
+};
---
+++ firmware/Core/Peripherals/Src/flash.cpp
@@ -0,0 +1,56 @@
+#include "flash.hpp"
+#include "main.h"
+#include <string.h>
+
+void Flash::erase(void){
+    HAL_FLASH_Unlock();
+
+    FLASH_EraseInitTypeDef eraseproperty;
+    
+    eraseproperty.TypeErase = FLASH_TYPEERASE_SECTORS;
+    eraseproperty.Sector = FLASH_SECTOR_1;
+    eraseproperty.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+    eraseproperty.NbSectors = 1;
+
+    uint32_t error_sector;
+    HAL_FLASHEx_Erase(&eraseproperty, &error_sector);
+    
+    HAL_FLASH_Lock();
+}
+
+void Flash::save(uint8_t (&wall_data)[16][16]){
+    erase();
+
+    HAL_FLASH_Unlock();
+
+    uint32_t *wall_data_address = (uint32_t*) wall_data;
+
+    const size_t write_count = sizeof(uint8_t) * 256 / sizeof(uint32_t);
+
+    for (size_t i = 0; i < write_count; i++){
+        HAL_FLASH_Program(
+            FLASH_TYPEPROGRAM_WORD,
+            (uint32_t)(_start_address) + sizeof(uint32_t) * i,
+            wall_data_address[i]
+        );
+    }
+
+    HAL_FLASH_Lock();
+}
+
+void Flash::load(void){
+    
+    memcpy(wall, _start_address, sizeof(wall));
+    
+    // 消去後と仮定する(消去後であればすべて1)
+    is_elased = 1;
+
+    for (int i = 0; i < 16; i++){
+        for (int j = 0; j < 16; j++){
+            if(wall[i][j] == 0){
+                // 0があれば消去前(消去後であればすべて1)
+                is_elased = 0;
+            }
+        }
+    }
+}
